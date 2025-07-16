6cca18b10acb148552bb6caa4dc5cad759faf2fe
Paulo Henrique Bomfim <paulo.bomfim@lekto.com.br>
2025-07-15 21:00:04 -0300

    feat: add task form page layout with reusable components and styles

--- src/app/pages/task-form/task-form.ts
+++ src/app/pages/task-form/task-form.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import {Input} from '../../components/input/input';
+import {Button} from '../../components/button/button';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-task-form',
-  imports: [],
+  imports: [
+    Input,
+    Button,
+    RouterLink,
+  ],
   templateUrl: './task-form.html',
   styleUrl: './task-form.css'
 })
